188571c45b42ae18d9aba4ce0f7b0551baa2a272
phoneypatjordan <87384359+phoneypatjordan@users.noreply.github.com>
2021-07-13 19:01:20 +0200

    Add files via upload

---
+++ exercises/137-shopping-list/script.js
@@ -0,0 +1,54 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var li = document.querySelectorAll('li');
+
+function inputLength() {
+	return input.value.length;
+}
+
+function addButton(li){
+	var deleteButton = document.createElement("Button");
+	deleteButton.innerHTML = "Delete";
+	li.appendChild(deleteButton);
+	deleteButton.onclick=removeParent;
+	input.value = "";
+}
+
+function createListElement() {	
+	var item = document.createElement("li");
+	item.appendChild(document.createTextNode(input.value));
+	ul.appendChild(item);
+
+	addButton(item);
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+function removeParent(){
+	this.parentNode.remove();		
+}
+
+function doneToggle(event){		
+	if (event.target.tagName === "LI") {
+		event.target.classList.toggle("done");	
+		}		
+}
+
+li.forEach(addButton);
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
+ul.addEventListener("click", doneToggle);
